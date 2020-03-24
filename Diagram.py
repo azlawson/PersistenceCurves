8ecef6283a6580f908011506500837d8c45e453b
azlawson <azlawson@uncg.edu>
2020-03-23 21:26:10 -0400

    Added Gaussian PCs

--- Diagram.py
+++ Diagram.py
@@ -7,17 +7,24 @@ Created on Mon Nov  5 11:26:12 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy import stats
 class Diagram:
-    def __init__(self, Dgm, globalmaxdeath = None, infinitedeath=-1):
+    def __init__(self, Dgm, globalmaxdeath = None, infinitedeath=-1, inf_policy="keep"):
         #@param Dgm: A persistence Diagram, i.e. an n by 2 array
         #@param globalmaxdeath: The maximum possible death value for the persistence process, e.g. in the case of 8-bit images, the max death is 255. Leave as None if there is no such value
         #@param infinitedeath: The value that represents an infinite death value, e.g. for Perseus that value is -1, which is default for this package
-        
-        self.Birth = np.array(Dgm)[:,0]
-        self.Death = np.array(Dgm)[:,1]
         self.globalmaxdeath = globalmaxdeath
         self.infinitedeath = infinitedeath
+        self.Birth = np.array(Dgm)[:,0]
+        self.Death = np.array(Dgm)[:,1]
+        if inf_policy=="remove":
+            self.Birth = self.Birth[self.Death != self.infinitedeath]
+            self.Death = self.Death[self.Death != self.infinitedeath]
+        elif inf_policy=="keep":
+            if self.globalmaxdeath is None:
+                self.Death[self.Death==self.infinitedeath] =np.max(self.Death)
+            else:
+                self.Death[self.Death==self.infinitedeath] = self.globalmaxdeath
         self.shape = Dgm.shape
         self.diagram = np.stack([self.Birth, self.Death], axis = 1)
 
@@ -29,12 +36,12 @@ class Diagram:
             finBirth = Birth[Death!=self.infinitedeath]
             finDeath = Death[Death!=self.infinitedeath]
             infBirth =Birth[Death==self.infinitedeath]
-            infDeath =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            infDeath =Death[Death==self.infinitedeath] + np.max(self.Death)
         else:
             finBirth = Birth[Death!=self.infinitedeath]
             finDeath = Death[Death!=self.infinitedeath]
             infBirth =Birth[Death==self.infinitedeath]
-            infDeath = Death[Death==self.infinitedeath] +2 + self.globalmaxdeath
+            infDeath = Death[Death==self.infinitedeath] + self.globalmaxdeath
         plt.scatter(finBirth, finDeath, s=ptsize)
         plt.scatter(infBirth, infDeath, marker = "D", c='red')
         plt.plot([np.min(self.Birth) - 1, np.max(self.Death) + 1], [np.min(self.Birth) - 1, np.max(self.Death) + 1], c="green")
@@ -295,4 +302,220 @@ class Diagram:
             else:
                 Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath] +2 + self.globalmaxdeath
         return np.sum(Death/Birth)
-    
+    def smooth_life(self, meshstart, meshstop, num_in_mesh, spread = 1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death - Birth)/np.max(Death-Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B)/spread)*(1-stats.norm.cdf((T-De)/spread)), axis=0)
+    def smooth_life_derivative(self, meshstart, meshstop, num_in_mesh, spread = 1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death - Birth)/np.max(Death-Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*(stats.norm.pdf((T-B)/spread) - stats.norm.pdf((T-B)/spread)*(stats.norm.cdf((T-De)/spread))-stats.norm.pdf((T-De)/spread)*(stats.norm.cdf((T-B)/spread))), axis=0)
+    def smooth_Betti(self, meshstart, meshstop, num_in_mesh,spread=1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = np.ones(Birth.shape[0])/Birth.shape[0]
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B)/spread)*(1-stats.norm.cdf((T-De)/spread)), axis=0)
+    def smooth_midlife(self, meshstart, meshstop, num_in_mesh,spread=1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Birth+Death)/np.max(Birth+Death)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B)/spread)*(1-stats.norm.cdf((T-De)/spread)), axis=0)
+    def smooth_midlife_derivative(self, meshstart, meshstop, num_in_mesh, spread = 1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death + Birth)/np.max(Death+Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*(stats.norm.pdf((T-B)/spread) - stats.norm.pdf((T-B)/spread)*(stats.norm.cdf((T-De)/spread))-stats.norm.pdf((T-De)/spread)*(stats.norm.cdf((T-B)/spread))), axis=0)
+    def smooth_multilife(self, meshstart, meshstop, num_in_mesh,spread=1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death/Birth)/np.max(Death/Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B)/spread)*(1-stats.norm.cdf((T-De)/spread)), axis=0)
+    def smooth_lifeentropy(self, meshstart, meshstop, num_in_mesh,spread=1):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death-Birth)/np.max(Death-Birth)
+        Life = Life*np.log(Life)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B)/spread)*(1-stats.norm.cdf((T-De)/spread)), axis=0)
+    def smooth_life_sum(self, meshstart, meshstop, num_in_mesh):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death - Birth)/np.sum(Death-Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B))*(1-stats.norm.cdf((T-De))), axis=0)
+    def smooth_midlife_sum(self, meshstart, meshstop, num_in_mesh):
+        #Produces the gaussian life curve of the diagram
+        #@param meshstart: The lowest value at which to begin the curve
+        #@param meshstop: the highest value at which to stop the curve
+        #@param num_in_mesh: The number of evenly spaced points between meshstart and meshstop at which to compute the curve values
+        Birth = self.Birth
+        Death = self.Death
+        if self.infinitedeath is None:
+            if self.globalmaxdeath is None:
+                Death[Death<0] =Death[Death<0] + np.max(self.Death) + 2
+            else:
+                Death[Death<0] = Death[Death<0] +2 + self.globalmaxdeath
+        else:
+            if self.globalmaxdeath is None:
+                Death[Death==self.infinitedeath] =Death[Death==self.infinitedeath] + np.max(self.Death) + 2
+            else:
+                Death[Death==self.infinitedeath] = Death[Death==self.infinitedeath]+2 + self.globalmaxdeath
+        T = np.linspace(meshstart, meshstop, num_in_mesh)
+
+        Life = (Death + Birth)/np.sum(Death+Birth)
+        B = np.matmul(np.array(Birth).reshape(-1,1),np.ones([1,num_in_mesh]))
+        De = np.matmul(np.array(Death).reshape(-1,1),np.ones([1,num_in_mesh]))
+        L = np.matmul(np.array(Life).reshape(-1,1),np.ones([1,num_in_mesh]))
+        return np.sum(L*stats.norm.cdf((T-B))*(1-stats.norm.cdf((T-De))), axis=0)
