0f360325eed25f58ef93e3ffeaaa4c7657bf2e60
azlawson <azlawson@uncg.edu>
2020-04-27 16:51:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.txt", "r") as fh:
 
 setuptools.setup(
     name="persistencecurves",
-    version="0.0.1",
+    version="0.0.2",
     author="Austin Lawson",
     author_email="azlawson@uncg.edu",
     description="A small package created to aid in the calculation of Persistence Curves",
