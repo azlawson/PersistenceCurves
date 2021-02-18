c1c8807185825d60c60c993ee912067da1fcef57
azlawson <azlawson@uncg.edu>
2021-02-18 18:53:55 -0500

    Fixed some issues with entropy functions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.txt", "r") as fh:
 
 setuptools.setup(
     name="persistencecurves",
-    version="0.0.5",
+    version="0.0.7",
     author="Austin Lawson",
     author_email="azlawson@uncg.edu",
     description="A small package created to aid in the calculation of Persistence Curves",
